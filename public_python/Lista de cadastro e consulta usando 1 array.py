0b60fb7e0ced331c0cb2bf63c6052e54a4c148da
Gabriel Camargo <gabriel.cleite@outlook.com>
2019-10-11 13:46:52 -0300

    Comentarios adicionados.

--- public_python/Lista de cadastro e consulta usando 1 array.py
+++ public_python/Lista de cadastro e consulta usando 1 array.py
@@ -1,4 +1,4 @@
-#   1) Faça um programa de Agenda usando Lista.
+#   1) Faça um programa de Agenda usando Lista bidimensional.
 #      O programa deve ter um menu que permita:
 #           
 #           1- Cadastrar
@@ -14,83 +14,82 @@
 #                          |  del lista[index]              |
 #-------------------------------------------------------------------------
 
-Contatos= []
-x= str('')
-while True:
+Contatos= []                                                # Declaração de vetor
+x= str('')                                                  # Declaração de variavel
+while True:                                                 # Criando um Looping com possibilidade de finaçização do programa
     
-    print (18* "__")     # Quebra de linha para melhor visualização :)
-    print ()             #
+    print (18* "__")                                        # Linha para dividir etapas do programa
+    print ()                                                # Quebra de linha para melhor visualização
     
-    print ("1) Cadastrar.")
-    print ("2) Consultar.")
-    print ("3) Mostrar todos os contatos")
-    print ("4) Sair")
+    print ("1) Cadastrar.")                                 # Opções para o usuário
+    print ("2) Consultar.")                                 # Opções para o usuário
+    print ("3) Mostrar todos os contatos")                  # Opções para o usuário
+    print ("4) Sair")                                       # Opções para o usuário
 
-    print ()
+    print ()                                                # Quebra de linha para melhor visualização
 
-    opcao= int(input("Digite uma opcao para avançar: "))
+    opcao= int(input("Digite uma opcao para avançar: "))    # Opções para o usuário
 
 #--------------------------------------------------------------------------
 
-    if opcao == 1:
-        nome =str(input("Nome: ")).lower()
-        tel = str(input("Tel: "))
+    if opcao == 1:                          # Opção Cadastrar Contato
+        nome =str(input("Nome: ")).lower()  # Nome do Contato
+        tel = str(input("Tel: "))           # Telefone do Contato
         
-        linha= [nome,tel]
-        Contatos.append(linha)
+        linha= [nome,tel]                   # Concatenação das informações em um vetor
+        Contatos.append(linha)              # Adição do vetor dentro da lista Contatos (Vetor bidimensional)
         
 #--------------------------------------------------------------------------
 
-    elif opcao == 2:
-        Consulta= str(input("Digite o nome do contato: ")).lower()
-        for i in range(len(Contatos)):    
-            if(i >= len(Contatos)):
-                break		#evitando erro de out of range in "i".
+    elif opcao == 2:                                                        # Opção Consultar contato                                      
+        Consulta= str(input("Digite o nome do contato: ")).lower()          # Pesquisa do Usuário ("lower" para nao haver incompatibilidade com letras maiusculas e minusculas)
+        for i in range(len(Contatos)):                                      # varredura em INT para index do array
+            if(i >= len(Contatos)):                                         # Condição de variável i for maior o comprimento do array (out of range)
+                break		                                                # Evitando erro de out of range in "i"
             
-            for z in (Contatos[i]):
+            for z in (Contatos[i]):                                         # Varredura de string (nome) para comparação da condição seguinte
                 #print (z)  <-- teste de comportamento
-                if z == Consulta:
-                    print (f'Nome: {Contatos[i][0]}')
-                    print (f'Tel: {Contatos[i][1]}')
-                    print ()
+                if z == Consulta:                                           # Condição de nome pesquisado = nome na lista Contatos
+                    print (f'Nome: {Contatos[i][0]}')                       # Se Condição verdadeira, Mostrar nome pesquisado
+                    print (f'Tel: {Contatos[i][1]}')                        # Se Condição verdadeira, Mostrar Telefone pesquisado
+                    print ()                                                # Quebra de linha
                         
-                    print ("1) Alterar dados do Contato.")
-                    print ("2) Excluir contato.")
-                    print ("3) Voltar ao inicio.")
+                    print ("1) Alterar dados do Contato.")                  # Opções do menu de alteração da lista Contatos
+                    print ("2) Excluir contato.")                           # Opções do menu de alteração da lista Contatos
+                    print ("3) Voltar ao inicio.")                          # Opções do menu de alteração da lista Contatos
                                 
-                    opcao2= int(input("Digite uma opção para continuar: "))# Selecionar uma opçao acima desta instrução
+                    opcao2= int(input("Digite uma opção para continuar: ")) # Seleciona uma opçao acima desta instrução
+                    while opcao2!=1 and opcao2!=2 and opcao2!=3:  	        # Opção inexistente das que estavam disponíveis (menu de alteração)
+                        print("Opção inexistente!")                         # Mensagem de Opcao inválida
+                        opcao2= int(input("Digite a opção novamente: "))    # Solicita ao usuário para digitar novamente
 
-                    while opcao2!=1 and opcao2!=2 and opcao2!=3:  	# Opção inexistente das que estavam disponíveis (menu de alteração)
-                        print("Opção inexistente!")          
-                        opcao2= int(input("Digite a opção novamente: "))
-
-                    if (opcao2 == 1):       # Opção Alterar dados do Contato
-                        Contatos[i][0]= str(input("Nome: ")).lower()
-                        Contatos[i][1]= str(input("Tel: "))
+                    if (opcao2 == 1):                                       # Opção Alterar dados do Contato
+                        Contatos[i][0]= str(input("Nome: ")).lower()        # Alteração do Nome previamente soliciado
+                        Contatos[i][1]= str(input("Tel: "))                 # Alteração do Telefone previamente solicitado
                         
-                    elif (opcao2 == 2):     # Opção Remover contato
-                        Contatos.pop(i)
-                        break
+                    elif (opcao2 == 2):                                     # Opção Remover contato
+                        Contatos.pop(i)                                     # Remoção do Contato e seu respectivo telefone (i = linha do vetor)
+                        break                                               # "Break" para quebrar a rotina de varredura "for" e evitar o erro "out of range"
 
-                    elif (opcao2 == 3):		# Volta para o menu de seleção
-                        print ("Voltando ao inicio...")  
+                    elif (opcao2 == 3):		                                # Opção retornar para o menu
+                        print ("Voltando ao inicio...")                     # Mensagem de retorno
                  
 #--------------------------------------------------------------------------
                     
-    elif opcao == 3:                            # Mostrar todos os Contatos.
-        print ("__"* 18,"\n")
-        print ("AGENDA")
+    elif opcao == 3:                            # Mostrar todos os Contatos
+        print ("__"* 18,"\n")                   # Traço para dividir etapas
+        print ("AGENDA")                        # Titulo AGENDA
         print ("----")
-        for x in range(len(Contatos)):          # Varredura na lista contatos.
-            print (f'Contato {x+1}:')
-            print (f'Nome: {Contatos[x][0]}')
-            print (f'Tel: {Contatos[x][1]}')
-            print ("----")            
+        for x in range(len(Contatos)):          # Varredura na lista Contatos.
+            print (f'Contato {x+1}:')           # Contato de numero (1,2,...,N)   x+1 para não ter "Contato 0"
+            print (f'Nome: {Contatos[x][0]}')   # Nome: celula de linha variavel e coluna constante no vetor
+            print (f'Tel: {Contatos[x][1]}')    # Tel: celula de linha variavel e coluna constante no vetor
+            print ("----")                      # Divisória de Contatos
 
 #--------------------------------------------------------------------------
 
     elif opcao == 4:    # Sair do Programa
-        break           # Interrompe o Loop while True (rotina infinita do código).
+        break           # Interrompe o Loop while True (loop infinito do código)
 
 #-------------------------------------------------------------------------
     
--- public_python/Notas de alunos e media da turma.py
+++ public_python/Notas de alunos e media da turma.py
@@ -4,53 +4,56 @@
 #   Mostre tambem a media da turma e o numero de aprovados e reprovados.
 #-----------------------------------------------------------------------------
 
-somaT= float(0)
-Aprov= int(0)
-Reprov= int(0)
-Alunos= []
+somaT= float(0)                                                 # Declaração de variavel 
+Aprov= int(0)                                                   # Declaração de variavel
+Reprov= int(0)                                                  # Declaração de variavel
+Alunos= []                                                      # Declaração de array
+
+
+for x in range(5):                                              # Repetição para estruturação de dados
+    print ("----")                                              # Divisória de alunos
+    nome= str(input("Nome: "))                                  # Nome do aluno
+    nota1= float(input("1a Nota: "))                            # Nota 1
+    nota2= float(input("2a Nota: "))                            # Nota 2
+    nota3= float(input("3a Nota: "))                            # Nota 3
+    nota4= float(input("4a Nota: "))                            # Nota 4
+    media= float((nota1 + nota2 + nota3 + nota4)/4)             # Calculo da media 
+    print(f'Media: {media}\n')                                  # Mostrando a media do aluno
+    linha= [nome, nota1, nota2, nota3, nota4, media]            # Concatenando informações do aluno em um vetor
+    Alunos.append(linha)                                        # Adição das informação em um vetor bidirecional
+print ("__" * 9)                                                # Linha dividindo etapas do código
+print ("TURMA \n")                                              # Titulo AGENDA
+
+
+
+for x in range(len(Alunos)):                                    # Varredura com x= INT para index do vetor
+    print (f'Aluno: {Alunos[x][0]}')                            # Apresentação do Nome do aluno
+    print (f'1a nota: {Alunos[x][1]}')                          # Apresentação da Nota 1 do aluno
+    print (f'2a nota: {Alunos[x][2]}')                          # Apresentação da Nota 2 do aluno
+    print (f'3a nota: {Alunos[x][3]}')                          # Apresentação da Nota 3 do aluno
+    print (f'4a nota: {Alunos[x][4]}')                          # Apresentação da Nota 4 do aluno
+    if Alunos[x][5] >= 7:                                       # Condição se a media do aluno for maior ou igual a 7
+        Aprov+=1                                                # Contador de alunos aprovados
+        print (f'Media obitida: {Alunos[x][5]} - Aprovado')     # Apresentação da média do aluno e seu Status
+        print ("----")                                          # Linha divisória de alunos
+    else:                           
+        Reprov+=1                                               # Contador de alunos Reprovados
+        print (f'Media obitida: {Alunos[x][5]} - Reprovado')    # Apresentação da média do aluno e seu Status
+        print ("----")                                          # Linha divisória de alunos
+
+
+
+print ("__" * 9)                                                # Linha dividindo etapas do código
+print ("MÉDIA GERAL")                                           # Titulo MÉDIA GERAL
+print ("----")                                                  # Linha divisória de alunos
+for x in range (len(Alunos)):                                   # varredura com x = INT para index do vetor
+    somaT= float(somaT + Alunos[x][5])                          # Calculo de soma das medias da turma
+    mediaT= float(somaT/len(Alunos))                            # Calculo da media da turma
     
-for x in range(5):
-    nome= str(input("Nome: "))
-    nota1= float(input("1a Nota: "))
-    nota2= float(input("2a Nota: "))
-    nota3= float(input("3a Nota: "))
-    nota4= float(input("4a Nota: "))
-    media= float((nota1 + nota2 + nota3 + nota4)/4)
-
-    linha= [nome, nota1, nota2, nota3, nota4, media]
-    Alunos.append(linha)
-
-print ("__" * 9)
-print ("TURMA \n")
-print("----")
-
-for x in range(len(Alunos)):
-    print (f'Aluno: {Alunos[x][0]}')
-    print (f'1a nota: {Alunos[x][1]}')
-    print (f'2a nota: {Alunos[x][2]}')
-    print (f'3a nota: {Alunos[x][3]}')
-    print (f'4a nota: {Alunos[x][4]}')
-    if Alunos[x][5] >= 7:
-        Aprov+=1
-        print (f'Media obitida: {Alunos[x][5]} - Aprovado')
-        print ("----")
-    else:
-        Reprov+=1
-        print (f'Media obitida: {Alunos[x][5]} - Reprovado')
-        print ("----")
-    print ("----")
-
-print ("__" * 9)
-print ("MÉDIA GERAL")
-print ("----")
-for x in range (len(Alunos)):
-    somaT= float(somaT + Alunos[x][5])
-    mediaT= float(somaT/len(Alunos))
-
-print (f'Média obtida pela Turma: {mediaT}')
-print (f'Aprovados: {Aprov}\nReprovados: {Reprov}')
-print("----")
-print ("__" * 9)
+print (f'Média obtida pela Turma: {mediaT}')                    # Média obtida pela turma
+print (f'Aprovados: {Aprov}\nReprovados: {Reprov}')             # Contabilização de alunos aprovados e reprovados
+print("----")                                                   # Linha divisória de alunos
+print ("__" * 9)                                                # Linha Final do código
 
 
 
